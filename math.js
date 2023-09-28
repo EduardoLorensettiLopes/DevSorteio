a1b5a1e4cbb50c9c0c1ef0fd24215921e69bdceb
Eduardo Lorensetti Lopes <eduardolorensetti@gmail.com>
2023-09-28 17:38:31 -0300

    devsorteio

---
+++ math.js
@@ -0,0 +1,10 @@
+function generateNumber(){
+    
+    const min = Math.ceil(document.querySelector("#n1").value)
+
+    const max = Math.floor(document.querySelector("#n2").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1) + min)
+    
+    alert(result)
+}
