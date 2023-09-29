fbf5d678713eda4a47bff6786428b538182ef046
Eduardo Lorensetti Lopes <eduardolorensetti@gmail.com>
2023-09-29 09:43:23 -0300

    atualizacao

--- math.js
+++ math.js
@@ -4,7 +4,10 @@ function generateNumber(){
 
     const max = Math.floor(document.querySelector("#n2").value)
 
+    const resultP = document.querySelector(".result")
+
     const result = Math.floor(Math.random() * (max - min + 1) + min)
     
-    alert(result)
+    resultP.innerHTML = result
+    
 }
